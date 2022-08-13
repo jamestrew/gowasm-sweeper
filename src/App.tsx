b9e6f2a874ab6d2fff42f268036ed3538ab4d9f7
James Trew <j.trew10@gmail.com>
2022-08-13 12:14:37 -0400

    feat: add cell icons and colors

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
 							x={j}
 							y={i}
 							key={i * width + j}
-							board={board}
+							cellType={board[j][i]}
 							openCell={() => setBoard(JSON.parse(window.openCell(j, i)))}
 						/>
 					))
@@ -37,11 +37,38 @@ function App() {
 type CellProps = {
 	x: number;
 	y: number;
-	board: number[][];
+	cellType: number;
 	openCell: () => void;
 };
 
-const Cell = ({ x, y, board, openCell }: CellProps) => {
+const Cell = ({ x, y, cellType, openCell }: CellProps) => {
+  let cellIcon: string;
+
+  switch (cellType) {
+    case -1:
+    case 0:
+      cellIcon = "";
+      break;
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+      cellIcon = cellType.toString();
+      break;
+    case 9:
+      cellIcon = "💣"
+      break;
+    case -2:
+      cellIcon = "🚩"
+      break;
+    default:
+      throw new Error(`undefined cell type ${cellType}`)
+  }
+
 	return (
 		<div
 			className="Cell"
@@ -50,11 +77,11 @@ const Cell = ({ x, y, board, openCell }: CellProps) => {
 				height: `${CELL_SIZE - 6}px`,
 				gridColumnStart: x + 1,
 				gridRowStart: y + 1,
-				background: "white",
-			}}
+				background: `${cellType === -1 ? 'white' : ' #7f8c8d '}`,
+      }}
 			onClick={openCell}
 		>
-			{`${board[y][x]}`}
+			{cellIcon}
 		</div>
 	);
 };
