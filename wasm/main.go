9912df86bba244cb7c41ae63c39ae5694ee3ba2e
James Trew <j.trew10@gmail.com>
2022-08-12 22:51:06 -0400

    refactor: wasm clean up

--- wasm/main.go
+++ wasm/main.go
@@ -8,29 +8,26 @@ import (
 
 var game *g.Game
 
-func newGame() js.Func {
-	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		difficulty := g.DifficultyLevel(args[0].Int())
-		g.CustomWidth = args[1].Int()
-		g.CustomHeight = args[2].Int()
-		g.CustomMineCount = args[3].Int()
-		game, _ = g.NewGame(difficulty)
-		game.FillMines()
-		return game.AsJson()
-	})
+func newGame(this js.Value, args []js.Value) interface{} {
+	difficulty := g.DifficultyLevel(args[0].Int())
+	g.CustomWidth = args[1].Int()
+	g.CustomHeight = args[2].Int()
+	g.CustomMineCount = args[3].Int()
+	game, _ = g.NewGame(difficulty)
+	// TODO: create random seed
+	game.FillMines()
+	// game.CalcAllNeighbors()
+	return game.AsJson()
 }
 
-func openCell() js.Func {
-	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		x, y := args[0].Int(), args[1].Int()
-		game.OpenCell(x, y)
-		return game.AsJson()
-	})
+func openCell(this js.Value, args []js.Value) interface{} {
+	x, y := args[0].Int(), args[1].Int()
+	game.OpenCell(x, y)
+	return game.AsJson()
 }
 
 func main() {
-	ch := make(chan struct{}, 0)
-	js.Global().Set("newGame", newGame())
-	js.Global().Set("openCell", openCell())
-	<-ch
+	js.Global().Set("newGame", js.FuncOf(newGame))
+	js.Global().Set("openCell", js.FuncOf(openCell))
+	<-make(chan bool)
 }
