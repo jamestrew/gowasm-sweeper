7b7fe3d615d9dfccdd3c58deaabb02bdceedb1b7
James Trew <j.trew10@gmail.com>
2022-08-14 21:32:12 -0400

    feat(ui): add cell colors

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -20,7 +20,7 @@ const Cell = ({
 	chordedOpen,
 }: CellProps) => {
 	const [clickDown, setClickDown] = useState<number>();
-	const { icon, background } = cellIcon(cellType);
+	const { icon, background, color } = cellIcon[cellType];
 
 	const sendAction = () => {
 		switch (clickDown) {
@@ -45,6 +45,8 @@ const Cell = ({
 				gridColumnStart: x + 1,
 				gridRowStart: y + 1,
 				background: background,
+				fontWeight: "525",
+				color: color,
 			}}
 			onContextMenu={(e) => e.preventDefault()}
 			onMouseDown={(e) => setClickDown(e.buttons)}
@@ -55,39 +57,21 @@ const Cell = ({
 	);
 };
 
-const cellIcon = (cellType: number): { icon: string; background: string } => {
-	let icon: string;
-	let background = "#7f8c8d";
-
-	switch (cellType) {
-		case -1:
-			icon = "";
-			background = "white";
-			break;
-		case 0:
-			icon = "";
-			break;
-		case 1:
-		case 2:
-		case 3:
-		case 4:
-		case 5:
-		case 6:
-		case 7:
-		case 8:
-			icon = cellType.toString();
-			break;
-		case 9:
-			icon = "💣";
-			break;
-		case -2:
-			icon = "🚩";
-			background = "white";
-			break;
-		default:
-			throw new Error(`undefined cell type ${cellType}`);
-	}
-	return { icon, background };
+const cellIcon: {
+	[cellType: string]: { icon: string; background: string; color: string };
+} = {
+	"-2": { icon: "🚩", background: "white", color: "" },
+	"-1": { icon: "", background: "white", color: "" },
+	"0": { icon: "", background: "#c0c0c0", color: "" },
+	"1": { icon: "1", background: "#c0c0c0", color: "#0000ff" },
+	"2": { icon: "2", background: "#c0c0c0", color: "#008200" },
+	"3": { icon: "3", background: "#c0c0c0", color: "#ff0000" },
+	"4": { icon: "4", background: "#c0c0c0", color: "#000084" },
+	"5": { icon: "5", background: "#c0c0c0", color: "#840000" },
+	"6": { icon: "6", background: "#c0c0c0", color: "#008284" },
+	"7": { icon: "7", background: "#c0c0c0", color: "#840084" },
+	"8": { icon: "8", background: "#c0c0c0", color: "#757575" },
+	"9": { icon: "💣", background: "#c0c0c0", color: "" },
 };
 
 export default Cell;
