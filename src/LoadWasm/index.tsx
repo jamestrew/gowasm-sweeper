bb74f5b149a24ead32b4de32239bf7005ab31c4b
James Trew <j.trew10@gmail.com>
2022-08-19 22:41:07 -0400

    style: format loadwasm

--- src/LoadWasm/index.tsx
+++ src/LoadWasm/index.tsx
@@ -1,12 +1,15 @@
-import './wasm_exec'
-import './wasmTypes.d.ts'
+import "./wasm_exec";
+import "./wasmTypes.d.ts";
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 async function loadWasm(): Promise<void> {
   const goWasm = new window.Go();
-  const result = await WebAssembly.instantiateStreaming(fetch('main.wasm'), goWasm.importObject);
-  goWasm.run(result.instance)
+  const result = await WebAssembly.instantiateStreaming(
+    fetch("main.wasm"),
+    goWasm.importObject
+  );
+  goWasm.run(result.instance);
 }
 
 export const LoadWasm: React.FC<React.PropsWithChildren<{}>> = (props) => {
@@ -15,16 +18,12 @@ export const LoadWasm: React.FC<React.PropsWithChildren<{}>> = (props) => {
   useEffect(() => {
     loadWasm().then(() => {
       setIsLoading(false);
-    })
+    });
   }, []);
 
   if (isLoading) {
-    return (
-      <div>
-        loading WebAssembly...
-      </div>
-    )
+    return <div>loading WebAssembly...</div>;
   } else {
-    return <React.Fragment>{props.children}</React.Fragment>
+    return <React.Fragment>{props.children}</React.Fragment>;
   }
-}
+};
